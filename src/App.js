cc2fb8b0a383350290fb476af125cc39a3fbc1d3
Micaiah Buttars <michael.buttars@gmail.com>
2019-07-16 11:18:09 -0600

    made it look cooler

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import test from './test.svg';
 import './App.scss';
-// import './App.css';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <span>Micaiah Buttars</span>
+      {/* <span className='name'>Micaiah Buttars</span> */}
     <div class='planets'>
       <div class='solar-syst'>
         <div class='sun'></div>
+        <div class='mercury-line'></div>
         <div class='mercury'></div>
         <div class='venus'></div>
         <div class='earth'></div> 
         <div class='mars'></div>
+        <div class='jupiter-inner'></div>
+        <div class='jupiter-outer'></div>
         <div class='jupiter'></div>
+
         <div class='saturn'></div>
         <div class='uranus'></div>
+        <div class='neptune-line'></div>
         <div class='neptune'></div>
       </div>
 
